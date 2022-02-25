73624afbabf64c43e25b8bf501680e02358448ed
Nikita Sokolov <snn0389@gmail.com>
2022-02-25 20:29:35 +0300

    search animation

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,11 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
+
+  icSearch = {
+    widthImg: 18000,
+    widthFrame: 150,
+    widthFrameElement: 98,
+    bgPosition: 0,
+    bgMaxPosition: 0,
+    framesNumber: 0,
+    duration: 26
+  }
+  
+  rearchInterval: any;
+
+  framesNumber: number;
 
   searchHints = [ 
     {
@@ -25,6 +39,19 @@ export class SearchComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.icSearch.framesNumber  = this.icSearch.widthFrame/this.icSearch.widthFrame;
+    this.icSearch.bgMaxPosition = -this.icSearch.widthFrame/this.icSearch.framesNumber*this.icSearch.widthFrameElement + this.icSearch.widthFrameElement * 31;
+    
+    this.rearchInterval = setInterval(() => {
+      if (this.icSearch.bgPosition > this.icSearch.bgMaxPosition) {
+        this.icSearch.bgPosition -= this.icSearch.widthFrameElement;
+      } else {
+        this.icSearch.bgPosition = 0;
+      }
+    }, this.icSearch.duration);
   }
 
+  ngOnDestroy() {
+    clearInterval(this.rearchInterval);
+  }
 }
