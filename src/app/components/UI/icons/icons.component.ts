070ca343ee3b99b20fb55475153b287ec34894f4
Nikita Sokolov <snn0389@gmail.com>
2022-03-03 10:55:40 +0300

    ui-icons fix

--- src/app/components/UI/icons/icons.component.ts
+++ src/app/components/UI/icons/icons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-icons',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./icons.component.scss']
 })
 export class IconsComponent implements OnInit {
-
+  @Input() id: string;
   constructor() { }
 
   ngOnInit(): void {
