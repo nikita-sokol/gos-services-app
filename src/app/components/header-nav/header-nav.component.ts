2aa950a63c42814254dca3bc0668f8945fcdd3cc
Nikita Sokolov <snn0389@gmail.com>
2022-03-10 11:46:32 +0300

    refactor: header-nav list

--- src/app/components/header-nav/header-nav.component.ts
+++ src/app/components/header-nav/header-nav.component.ts
@@ -6,7 +6,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-nav.component.scss']
 })
 export class HeaderNavComponent implements OnInit {
-
+  navList = [
+    {
+      text: 'Дом и ЖКХ',
+      link: '#',
+      icon: 'bulb',
+      class: 'bulb'
+    },
+    {
+      text: 'Здоровье',
+      link: '#',
+      icon: 'health',
+      class: 'health'
+    },
+    {
+      text: 'Земля<br>и стройка',
+      link: '#',
+      icon: 'crane',
+      class: 'crane'
+    },
+    {
+      text: 'Культура<br>и спорт',
+      link: '#',
+      icon: 'sport',
+      class: 'sport'
+    },
+    {
+      text: 'Личные документы',
+      link: '#',
+      icon: 'docs',
+      class: 'docs'
+    },
+    {
+      text: 'Образование',
+      link: '#',
+      icon: 'education',
+      class: 'education'
+    },
+    {
+      text: 'Соцподдержка',
+      link: '#',
+      icon: 'socsup',
+      class: 'socsup'
+    },
+    {
+      text: 'Транспорт',
+      link: '#',
+      icon: 'transport',
+      class: 'transport'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
