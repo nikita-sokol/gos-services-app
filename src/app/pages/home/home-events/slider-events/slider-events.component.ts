cd05be5722d8bdf9c9d32a6f72dd8c694eec3a99
Nikita Sokolov <snn0389@gmail.com>
2022-03-14 15:33:52 +0300

    ref: slider-event

--- src/app/pages/home/home-events/slider-events/slider-events.component.ts
+++ src/app/pages/home/home-events/slider-events/slider-events.component.ts
@@ -1,31 +1,21 @@
-import { AfterViewInit, Component, Input, OnInit, HostListener } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-slider-events',
   templateUrl: './slider-events.component.html',
   styleUrls: ['./slider-events.component.scss']
 })
-export class SliderEventsComponent implements OnInit, AfterViewInit {
-  @HostListener('window:resize') 
-  onResize() {
-    if (window.innerWidth <= 1240) {
-      this.media = 'tablet_1240'
-    } else {
-      this.media = ''
-    }
-  }
+export class SliderEventsComponent implements OnInit {
   @Input() slides: any;
   prevSlideID: any;
   currentSlideID = 0;
   nextSlideID = 1;
   lastSlideID: any;
-  media = '';
 
   touch = false;
-  pos = 0;
-  posUnits: any = '0px';
+  pos: any = '0px';
   trfRegExp = /[-0-9.]+(?=px)/;
-  transition = 'transform .65s, opacity .4s linear';
+  transitionCurSlide = 'transform .65s, opacity .4s linear';
   posInit = 0;
   posX1 = 0;
   posX2 = 0;
@@ -56,34 +46,25 @@ export class SliderEventsComponent implements OnInit, AfterViewInit {
   }
 
   pagination(id: any) {
-    if (id == this.currentSlideID) return;
-
-    // если все prev или next
-    this.currentSlideID = id;
-        
-    this.prevSlideID =
-        this.currentSlideID === 0 ? this.lastSlideID : this.currentSlideID - 1;
-    this.nextSlideID =
-        this.currentSlideID === this.lastSlideID ? 0 : this.currentSlideID + 1;
-  
-    // второй вариант
-    //// this.nextSlideID = id;
-    //// setTimeout(() => {
-    ////   this.currentSlideID = id;
-    ////   this.prevSlideID = this.currentSlideID === 0 ? this.lastSlideID : this.currentSlideID - 1;
-    ////   this.nextSlideID = this.currentSlideID === this.lastSlideID ? 0 : this.currentSlideID + 1;
-    //// }, 100)
+    if (id !== this.currentSlideID) {
+      this.currentSlideID = id;
+          
+      this.prevSlideID =
+          this.currentSlideID === 0 ? this.lastSlideID : this.currentSlideID - 1;
+      this.nextSlideID =
+          this.currentSlideID === this.lastSlideID ? 0 : this.currentSlideID + 1;
+    }
   }
 
   slideChangeCondition(mode: any, id: any) {
     if (mode == 'prev') {
       return (this.currentSlideID == 0) ? (id == this.lastSlideID) : 
-             (this.currentSlideID == 28) ? (id != 0 && id != 28) : 
+             (this.currentSlideID == this.lastSlideID) ? (id != 0 && id != this.lastSlideID) : 
              (id < this.currentSlideID);
     }
     if (mode == 'next') {
       return (this.currentSlideID == this.lastSlideID) ? (id == 0) : 
-             (this.currentSlideID == 0) ? (id != 0 && id != 28) : 
+             (this.currentSlideID == 0) ? (id != 0 && id != this.lastSlideID) : 
              (id > this.currentSlideID);
     }
     return false;
@@ -96,13 +77,13 @@ export class SliderEventsComponent implements OnInit, AfterViewInit {
 
   transformSlide(id: any) {
     return (id == this.prevSlideID) ? 'translateX(-103%)' :
-        (id == this.currentSlideID) ? 'translateX(' + this.posUnits + ')' :
+        (id == this.currentSlideID) ? 'translateX(' + this.pos + ')' :
         (id == this.nextSlideID) ? 'translateX(103%)' : 'translateX(0)'
   }
 
   transstionSlide(id: any) {
     return (id == this.prevSlideID) ? 'transform .65s, opacity .4s linear' :
-        (id == this.currentSlideID) ? this.transition :
+        (id == this.currentSlideID) ? this.transitionCurSlide :
         (id == this.nextSlideID) ? 'transform .65s, opacity .4s linear': 'none'
   }
 
@@ -112,34 +93,29 @@ export class SliderEventsComponent implements OnInit, AfterViewInit {
 
     this.posInit = event.clientX;
     this.posX1 = event.clientX;
-    this.transition = 'none';
+    this.transitionCurSlide = 'none';
   }
 
   swipeAction(event: any) {
     if (this.touch) {
       this.getEvent(event);
       this.posX2 = this.posX1 - event.clientX;
-      this.posUnits = +this.posUnits.match(this.trfRegExp) - this.posX2 + 'px';
+      this.pos = +this.pos.match(this.trfRegExp) - this.posX2 + 'px';
       this.posX1 = event.clientX;
     }
   }
 
   swipeEnd(event: any) {
     this.posFinal = this.posInit - this.posX1;
-    this.transition = 'transform .65s, opacity .4s linear';
-    this.posUnits = '0px';
-    // если мы тянули вправо, то уменьшаем номер текущего слайда
+    this.transitionCurSlide = 'transform .65s, opacity .4s linear';
+    this.pos = '0px';
+    
     if (this.posInit < this.posX1) {
       this.changeCurrentSlide('prev');
-    // если мы тянули влево, то увеличиваем номер текущего слайда
     } else if (this.posInit > this.posX1) {
       this.changeCurrentSlide('next');
     }
 
     this.touch = false;
-  }
-
-  ngAfterViewInit() {
-    this.onResize();
   }
 }
